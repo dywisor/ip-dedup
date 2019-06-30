de03f1b06cf71770ef7deab19afc45ce4ff2ca34
André Erdmann <dywi@mailerd.de>
2019-06-30 23:04:21 +0200

    add some datadir ip list files

--- mk/common_targets.mk
+++ mk/common_targets.mk
@@ -5,5 +5,25 @@ clean:
 	test ! -f '$(O)/ip-dedup' || rm -- '$(O)/ip-dedup'
 
 PHONY += install
-install:
+install: install-bin install-data
+
+PHONY += install-bin
+install-bin:
 	$(DOEXE) -- $(O)/ip-dedup $(DESTDIR)$(BINDIR)/ip-dedup
+
+PHONY += install-data
+install-data: install-data-ip4 install-data-ip6
+
+PHONY += install-data-ip4
+install-data-ip4:
+	$(DOINS) -- $(S_IPLIST)/inet/doc $(DESTDIR)$(IPDEDUP_DATADIR)/inet/doc
+	$(DOINS) -- $(S_IPLIST)/inet/link-local $(DESTDIR)$(IPDEDUP_DATADIR)/inet/link-local
+	$(DOINS) -- $(S_IPLIST)/inet/lo $(DESTDIR)$(IPDEDUP_DATADIR)/inet/lo
+	$(DOINS) -- $(S_IPLIST)/inet/priv $(DESTDIR)$(IPDEDUP_DATADIR)/inet/priv
+
+PHONY += install-data-ip6
+install-data-ip6:
+	$(DOINS) -- $(S_IPLIST)/inet6/doc $(DESTDIR)$(IPDEDUP_DATADIR)/inet6/doc
+	$(DOINS) -- $(S_IPLIST)/inet6/link-local $(DESTDIR)$(IPDEDUP_DATADIR)/inet6/link-local
+	$(DOINS) -- $(S_IPLIST)/inet6/lo $(DESTDIR)$(IPDEDUP_DATADIR)/inet6/lo
+	$(DOINS) -- $(S_IPLIST)/inet6/ula $(DESTDIR)$(IPDEDUP_DATADIR)/inet6/ula
