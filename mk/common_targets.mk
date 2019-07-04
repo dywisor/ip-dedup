ddfdb8ea1e4477191e3a244b9937bf1653915996
André Erdmann <dywi@mailerd.de>
2019-07-04 20:29:11 +0200

    add martians list

--- mk/common_targets.mk
+++ mk/common_targets.mk
@@ -19,6 +19,7 @@ install-data-ip4:
 	$(DOINS) -- $(S_IPLIST)/inet/doc $(DESTDIR)$(IPDEDUP_DATADIR)/inet/doc
 	$(DOINS) -- $(S_IPLIST)/inet/link-local $(DESTDIR)$(IPDEDUP_DATADIR)/inet/link-local
 	$(DOINS) -- $(S_IPLIST)/inet/lo $(DESTDIR)$(IPDEDUP_DATADIR)/inet/lo
+	$(DOINS) -- $(S_IPLIST)/inet/martians $(DESTDIR)$(IPDEDUP_DATADIR)/inet/martians
 	$(DOINS) -- $(S_IPLIST)/inet/priv $(DESTDIR)$(IPDEDUP_DATADIR)/inet/priv
 
 PHONY += install-data-ip6
@@ -26,4 +27,5 @@ install-data-ip6:
 	$(DOINS) -- $(S_IPLIST)/inet6/doc $(DESTDIR)$(IPDEDUP_DATADIR)/inet6/doc
 	$(DOINS) -- $(S_IPLIST)/inet6/link-local $(DESTDIR)$(IPDEDUP_DATADIR)/inet6/link-local
 	$(DOINS) -- $(S_IPLIST)/inet6/lo $(DESTDIR)$(IPDEDUP_DATADIR)/inet6/lo
+	$(DOINS) -- $(S_IPLIST)/inet6/martians $(DESTDIR)$(IPDEDUP_DATADIR)/inet6/martians
 	$(DOINS) -- $(S_IPLIST)/inet6/ula $(DESTDIR)$(IPDEDUP_DATADIR)/inet6/ula
